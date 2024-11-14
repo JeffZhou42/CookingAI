f2bc12e5f1fd08a9f9c024c53a2e5c02ac67953e
Zhaoyyyyyyyy <rayzhao05@gmail.com>
2024-11-14 00:59:08 -0500

    setput node and express done

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
+require('dotenv').config()
+
 const express = require("express")
 
 //creates express app
 const app = express()
 
+//middleware logs path and request
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+//routes
+app.get('/', (req, res) => {
+    res.json({mssg: "welcome to the app"})
+})
+
 //listen for requests
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("listening on port 3000")
 })
